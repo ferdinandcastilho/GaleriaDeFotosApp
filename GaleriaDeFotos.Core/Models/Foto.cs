ec907da93c2972a2fa9089d21f8d8b9035bf0731
Fernando Castilho <developer@fernandocastilho.net>
2022-09-16 23:15:28 -0300

    Add CreateHash Method on FotosDataService

--- GaleriaDeFotos.Core/Models/Foto.cs
+++ GaleriaDeFotos.Core/Models/Foto.cs
@@ -5,7 +5,7 @@ namespace GaleriaDeFotos.Core.Models;
 // Model for the SampleDataService. Replace with your own model.
 public partial class Foto : ObservableObject
 {
-    [ObservableProperty] private long _imageId;
+    [ObservableProperty] private string _imageId;
 
     [ObservableProperty] private Uri _imageUri;
 }
--- GaleriaDeFotos.Core/Services/FotosDataService.cs
+++ GaleriaDeFotos.Core/Services/FotosDataService.cs
@@ -1,4 +1,5 @@
-﻿using GaleriaDeFotos.Core.Contracts.Services;
+﻿using System.Security.Cryptography;
+using GaleriaDeFotos.Core.Contracts.Services;
 using GaleriaDeFotos.Core.Models;
 
 namespace GaleriaDeFotos.Core.Services;
@@ -7,14 +8,18 @@ public class FotosDataService : IFotosDataService
 {
     public async Task<IEnumerable<Foto>> GetPhotos()
     {
-        var list = new List<Foto>();
         var imagePath = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
 
         await Task.CompletedTask;
 
         return Directory.GetFiles(imagePath)
             .Where(file => Path.GetExtension(file) is ".png" or ".jpg")
-            .Select(file => new Foto { ImageId = list.Count, ImageUri = new Uri(file) })
+            .Select(file => new Foto { ImageId = CreateHash(file) , ImageUri = new Uri(file) })
             .ToList();
+    }
+
+    private static string CreateHash(string file)
+    {
+        return null;
     }
 }
