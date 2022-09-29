718fa3ffb59f1e8e1b4245c085a3fa007ca53222
Fernando Castilho <ferdinandcastilho@outlook.com>
2022-09-28 21:27:38 -0300

    Create Repository
    closes #12

---
+++ GaleriaDeFotos.Core/DataContext/IFotoRepository.cs
@@ -0,0 +1,11 @@
+﻿using GaleriaDeFotos.Core.Models;
+
+namespace GaleriaDeFotos.Core.DataContext;
+
+public interface IFotoRepository
+{
+    Task<FotoData> Insert(FotoData fotoData);
+    Task<FotoData> Update(FotoData fotoData);
+    Task<bool> Delete(FotoData fotoData);
+    Task ClearAll();
+}
