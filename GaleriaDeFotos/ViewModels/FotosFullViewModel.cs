04da554fb81260b865f16e75118dbad1a540564f
Maikeu Locatelli <maikramer@gmail.com>
2022-10-07 20:24:25 -0300

    Add Image Size, File Size and Creation Date to Details

--- GaleriaDeFotos/ViewModels/FotosFullViewModel.cs
+++ GaleriaDeFotos/ViewModels/FotosFullViewModel.cs
@@ -3,26 +3,80 @@ using CommunityToolkit.Mvvm.Input;
 using GaleriaDeFotos.Contracts.ViewModels;
 using GaleriaDeFotos.Core.Contracts.Services;
 using GaleriaDeFotos.Core.Models;
+using ImageProcessor;
 
 namespace GaleriaDeFotos.ViewModels;
 
 public partial class FotosFullViewModel : ObservableRecipient, INavigationAware
 {
     private readonly IFotosDataService _fotosDataService;
-    [ObservableProperty] private string _fotoHeight = string.Empty;
-
-    //Propriedades da Foto
-    [ObservableProperty] private string _fotoWidth = string.Empty;
     [ObservableProperty] private bool _isFavorite;
 
     [ObservableProperty] private bool _isShowingDetails;
     [ObservableProperty] private Foto? _item;
+
     [ObservableProperty] private Foto? _selectedFoto;
-    [ObservableProperty] private string _sizeString = string.Empty;
+    //Propriedades
 
     public FotosFullViewModel(IFotosDataService fotosDataService)
     {
         _fotosDataService = fotosDataService;
+    }
+
+    public string FotoSizeString
+    {
+        get
+        {
+            if (Item == null)
+            {
+                return string.Empty;
+            }
+
+            using var imageStream = File.OpenRead(Item.ImageUri.AbsolutePath);
+            using var imageFactory = new ImageFactory();
+            imageFactory.Load(imageStream).AutoRotate(); //takes care of ex-if
+            var height = imageFactory.Image.Height;
+            var width = imageFactory.Image.Width;
+
+            return $"{width} x {height}";
+        }
+        // ReSharper disable once ValueParameterNotUsed
+        set { }
+    }
+
+    public string FileSizeString
+    {
+        get
+        {
+            if (Item == null)
+            {
+                return string.Empty;
+            }
+
+            var fi = new FileInfo(Item.ImageUri.AbsolutePath);
+            var mb = fi.Length / 1000000;
+            return mb > 1.0
+                ? $"{fi.Length / 1000000.0:0.00} MBs"
+                : $"{fi.Length / 1000.0:0.00} kBs";
+        }
+        // ReSharper disable once ValueParameterNotUsed
+        set { }
+    }
+
+    public string FileCreatedString
+    {
+        get
+        {
+            if (Item == null)
+            {
+                return string.Empty;
+            }
+
+            var fi = new FileInfo(Item.ImageUri.AbsolutePath);
+            return $"{fi.CreationTime:HH:mm:ss dd/MM/yyyy}";
+        }
+        // ReSharper disable once ValueParameterNotUsed
+        set { }
     }
 
     #region INavigationAware Members
